14ecfb090456ff8cbb432a4fd6d96c3318ff51b9
nico604 <nicolas.girard.qc@gmail.com>
2020-01-28 11:58:11 -0500

    opt firebase

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,17 +1,8 @@
 import * as firebase  from 'firebase';
+import  config from './config'
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDTwQFnKkAUtk5lJh1D5RSTzSHBmAPwwHU",
-  authDomain: "disquecollection.firebaseapp.com",
-  databaseURL: "https://disquecollection.firebaseio.com",
-  projectId: "disquecollection",
-  storageBucket: "disquecollection.appspot.com",
-  messagingSenderId: "129599902804",
-  appId: "1:129599902804:web:87b214a7c4170f1880c9c1",
-  measurementId: "G-X3TDZ0GZKE"
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 firebase.analytics();
 
 export default firebase;
