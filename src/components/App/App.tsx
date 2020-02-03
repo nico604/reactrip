f3ea9a2a037298719badce183ab63efab390b326
nico604 <nicolas.girard.qc@gmail.com>
2020-02-03 17:35:20 -0500

    re up

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,17 +5,12 @@
 import  React, {Component} from 'react';
 //import * as ReactDOM from 'react-dom';
 //import './App.css';
+const App = () => (
+	<div>
+		<h1>Dorion</h1>
+	</div>
+)
 
-
-class App extends React.Component<any, any>  {
-  public render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
-}
 
 export default App;
   
